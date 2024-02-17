ce7034c885f3fc927c927460efe260991360c14a
Majid Golshadi <golshadi.majid@gmail.com>
2024-02-17 23:37:14 +0100

    update workflow

--- internal/storage/mysql/coordinator.go
+++ internal/storage/mysql/coordinator.go
@@ -100,7 +100,7 @@ func (c *coordinator) setRange(ctx context.Context, nodeID string, requestedRang
 	if err != nil {
 		return err
 	}
-	// nolint:staticcheck
+	// lint:ignore
 	defer tx.Rollback()
 
 	query := `INSERT INTO nodes_coordination_keys(key_id, value, version) VALUES (?,?,?)
